7bfef6090905fa1f62c5e5d9f65b776310917ec1
Scott Richmond <s.richmond@utoronto.ca>
2017-02-26 13:49:14 -0500

    Prepare for loader.

--- myTurtle.js
+++ myTurtle.js
@@ -1,7 +1,5 @@
-var goTurtle = () => {
-  learn('circle', repeat(360, forward(1).right(1)))
+learn('circle', repeat(360, forward(1).right(1)))
 
-  learn('flower', repeat(45, circle().right(8)))
+learn('flower', repeat(45, circle().right(8)))
 
-  repeat(4, flower().forward(100).right(90))
-}
+repeat(4, flower().forward(100).right(90))
